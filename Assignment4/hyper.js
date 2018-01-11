60a9c2ac9bc7f2c2af3fb386fe719af107d921fa
mrnaia <marina.menghetti@gmail.com>
2018-01-11 18:52:12 +0100

    Log In Button OK

--- Assignment4/hyper.js
+++ Assignment4/hyper.js
@@ -1,5 +1,10 @@
 function initWebPage() { //all global var declarations and eventListeners...
-
+  document.getElementById('connexionButton').addEventListener('click',connect);
 }
 
 initWebPage(); //init the page when the script is run
+
+function connect(){
+  var username = document.getElementById('username').value;
+  document.getElementById('logInHeader').innerHTML = username;
+}
