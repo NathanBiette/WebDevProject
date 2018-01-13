1e37eb3ade39bf015fe19749e9ca8b0af3358817
justineDelo <ju.nyc@hotmail.fr>
2018-01-13 22:46:11 +0100

    en train de préparer le bloc avec infos persos et comment les rentrer et les sauvegarder quand on rafraîchit la page

---
+++ Assignment4/profile.js
@@ -0,0 +1,34 @@
+
+if(localStorage.ageOutput){
+//alert(localStorage.ageOutput);
+document.getElementById("ageOutput").value=localStorage.ageOutput + " years old";
+
+}
+if(localStorage.genderOutput){
+document.getElementById("genderOutput").value=localStorage.genderOutput;//getItem("genderOutput");
+}
+if(localStorage.countryOutput){
+document.getElementById("countryOutput").value=localStorage.countryOutput;//getItem("countryOutput");
+}
+
+
+function infoPerso(){
+  document.getElementById("ageOutput").value=document.getElementById("age").value + " years old";
+  //alert(document.getElementById("ageOutput").value);
+  //localStorage.setItem("ageOutput",document.getElementById("ageOutput").value );
+  localStorage.ageOutput=document.getElementById("ageOutput").value;
+  if(document.getElementById("male").checked){
+    document.getElementById("genderOutput").value="male";
+
+  }
+  else{
+    document.getElementById("genderOutput").value="female";
+  }
+  localStorage.genderOutput=document.getElementById("genderOutput").value;
+  alert(document.getElementById("genderOutput").value);
+
+  //localStorage.setItem("genderOutput",document.getElementById("genderOutput").value );
+  document.getElementById("countryOutput").value=document.getElementById("country").value;
+    localStorage.countryOutput=document.getElementById("countryOutput").value;
+  //localStorage.setItem("countryOutput", document.getElementById("countryOutput").value);
+}
