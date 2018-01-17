4317736e27c90e6e41c133856e57f0bcbf7667c6
Gautier Dervaux <gautier_dervaux@hotmail.com>
2018-01-17 21:47:51 +0100

    fixed top bar

--- Assignment4/caroussel.js
+++ Assignment4/caroussel.js
@@ -10,7 +10,7 @@ $(".translate").click(function(){
   direction = $(this).attr("class").split(' ')[1];
   var remainder = Math.floor(translateX%mediaWidth);
   if(remainder==0){
-    if(direction=='left' && remainder==0){//-500px to transfor
+    if(direction=='left' && remainder==0){//+500px to transform left
       if(translateX<0){
         translateX += mediaWidth;
       }
@@ -18,7 +18,7 @@ $(".translate").click(function(){
         translateX = -mediaWidth*(numberMediaSlideshow-1);
       }
     }
-    if(direction=='right' && remainder==0){//-500px to transforf
+    if(direction=='right' && remainder==0){//-500px to transforfm right
       if(translateX> -mediaWidth*(numberMediaSlideshow-1)){
         translateX -=mediaWidth;
       }
