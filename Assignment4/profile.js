faa005ddfd8b7d008e19c1ce9a87fe20abbfe5dd
mrnaia <marina.menghetti@gmail.com>
2018-01-19 22:05:59 +0100

    Onglets OK

--- Assignment4/profile.js
+++ Assignment4/profile.js
@@ -78,7 +78,7 @@ if (localStorage.onglet) {
 /* VERSION PLUS RAPIDE A ECRIRE */
 
 var onglets = [$("#ong1"),$("#ong2"),$("#ong3"),$("#ong4")];
-var ongletsContent = [$("#firstOnglet"),$("#secondOnglet"),$("#thirdOnglet"),$("#fourthOnglet")];
+var ongletsContent = [$("#ong1Content"),$("#ong2Content"),$("#ong3Content"),$("#ong4Content")];
 if (localStorage.onglet) {
   onglets[localStorage.onglet - 1].addClass("active");
   onglets[localStorage.onglet - 1].siblings().removeClass("active");
