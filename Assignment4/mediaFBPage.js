4bbc161d9955e8d13068dd92bcaca6b8665ba1b3
mrnaia <marina.menghetti@gmail.com>
2018-01-24 00:08:51 +0100

    add function load on mediaFBPage.js

--- Assignment4/mediaFBPage.js
+++ Assignment4/mediaFBPage.js
@@ -1,10 +1,16 @@
-$(".checkAllButton").click(function() {
-  $(".filterPostsButton").prop("checked",true);
-});
-$(".uncheckAllButton").click(function() {
-  $(".filterPostsButton").prop("checked",false);
+$(function() {
+
+  $(".checkAllButton").click(function() {
+    $(".filterPostsButton").prop("checked",true);
+  });
+  $(".uncheckAllButton").click(function() {
+    $(".filterPostsButton").prop("checked",false);
+  });
+
+  $(".filterPostsButton").on("change",function() {
+    console.log($(this).attr("class"));
+  });
+
 });
 
-$(".filterPostsButton").on("change",function() {
-  console.log($(this).attr("class"));
-});
+alert(localStorage.username)
