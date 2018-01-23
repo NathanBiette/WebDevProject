942d617c0ea53d0dcdb3f1fb5f8c23beb8c74ab7
justineDelo <ju.nyc@hotmail.fr>
2018-01-23 23:40:30 +0100

    progres sur le lien contributions star wars

--- Assignment4/contributions.js
+++ Assignment4/contributions.js
@@ -4,12 +4,24 @@
 
 
 
-ajouteElement();
+ajouteElement(1);
 
-function ajouteElement() {
+function ajouteElement(official) {
   // crée un nouvel élément div
   // et lui donne un peu de contenu
   const nouveauDiv = document.createElement("div");
+    nouveauDiv.className="draggable firstOnglet contrib";
+  if (official==1){
+    nouveauDiv.className+=" official";
+  }
+  else{
+    nouveauDiv.className+=" fanArt";
+  }
+
+
+
+  alert(nouveauDiv.className);
+
   const nouveauContenu = document.createTextNode("Salutations !");
   nouveauDiv.appendChild(nouveauContenu); //ajoute le contenu au div
 
@@ -17,5 +29,5 @@ function ajouteElement() {
   const divActuel = document.getElementById("beginning");
 
   document.getElementById("ong1Content").insertBefore(nouveauDiv, divActuel);
-    alert("here");
+
 }
