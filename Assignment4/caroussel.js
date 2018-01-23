2cc9e653e0f588bb4f1157d06509c26e11a20557
Gautier Dervaux <gautier_dervaux@hotmail.com>
2018-01-24 00:00:47 +0100

    New Carrousels with more medias on one page cf mediaHomePage for exemple

--- Assignment4/caroussel.js
+++ Assignment4/caroussel.js
@@ -1,54 +1,42 @@
 /*--------------------------------------------Jquery version--------------------------------------*/
 function initCarousels(){
-  $(".mediaContainer").data("showingMediaIndexInSLideshow",0);
-  $(".mediaContainer").data("showingSlideshowIndex",0);
-  $(".translate").click(function(event){
+  $(".mediasContainer").data("showingMediaIndexInSLideshow",0);
+  $(".mediasContainer").data("showingContainerIndex",0);
+  $(".translate").on('click',function(event){
   event.stopPropagation();
   translateSlideshow($(this));});
 }
 function translateSlideshow(translateButton){
-  //var slideshowClass = translateButton.parent().parent().attr("class").split(' ')[1];
   var mediasContainer = translateButton.siblings(".mediasContainer");
-  var mediaContainer = translateButton.siblings(".mediasContainer").children(".mediaContainer")
-  var numberMediaSlideshow = mediaContainer.length;
-  var showingSlideshowIndex = mediaContainer.data("showingSlideshowIndex");
-  var showingMediaIndex = mediaContainer.data("showingMediaIndexInSLideshow");
-  var showingSlideshow = mediaContainer.children().eq(showingSlideshowIndex)
-  var numberMediaInSlideshow = parseFloat(showingSlideshow.attr("data-nbMedia"));
+  var numberOfMediaContainer = mediasContainer.children(".mediaContainer").length;
+  var showingContainerIndex = mediasContainer.data("showingContainerIndex");
+
+  var mediaContainer = translateButton.siblings(".mediasContainer").children().eq(showingContainerIndex)
+  var numberMediaInContainer = mediaContainer.find(".media").length;
+  //console.log(numberMediaInContainer);
+  var showingMediaIndex = mediasContainer.data("showingMediaIndexInSLideshow");
+
   //Current translation
   var matrixString = mediasContainer.css("transform").split(',');
   var translateX = parseFloat(matrixString[matrixString.length-2]); //TranslationX
+
   var direction = translateButton.attr("class").split(' ')[1];
 
   //Caluclating the transition value to next media
-  console.log("Translation");
-  console.log(showingSlideshowIndex);
-  if(direction=='left'){// && remainder==0){//+500px to transform left
-    if(showingMediaIndex==0){ //To the left of the slideshow
-
-      if(showingSlideshowIndex==0){
-        showingSlideshowIndex=numberMediaSlideshow-1;// to the full begining of the slideshow
-        showingMediaIndex=0;
-      }
-      else{
-        showingSlideshow--;
-        showingMediaIndex=0;
-      }
-    }
-    else{
-      showingMediaIndex--;
-    }
-  }
-
+  //console.log("Translation");
+  //console.log(showingContainerIndex);
 
   if(direction=='right'){// && remainder==0){//+500px to transform left
-    if(showingMediaIndex==(numberMediaInSlideshow-1)){ //To the right of the slideshow
-      if(showingSlideshowIndex==(numberMediaSlideshow-1)){
-        showingSlideshowIndex=0;// to the full begining of the slideshow
+    //console.log("Test1");
+    //console.log(showingMediaIndex);
+    //console.log(numberMediaInContainer);
+    if(showingMediaIndex==(numberMediaInContainer-1)){ //To the right of the slideshow
+      if(showingContainerIndex==(numberOfMediaContainer-1)){
+        showingContainerIndex=0;// to the full begining of the slideshow
         showingMediaIndex=0;
       }
       else{
-        showingSlideshowIndex++;
+        showingContainerIndex++;
         showingMediaIndex=0;
       }
     }
@@ -57,27 +45,50 @@ function translateSlideshow(translateButton){
     }
 
   }
-  console.log(showingSlideshowIndex);
-  //html objects to be shown
-  showingSlideshow = mediaContainer.children().eq(showingSlideshowIndex);
-  showingMedia = showingSlideshow.find(".media").eq(showingMediaIndex);
-  //Caluclating the new translation
-  translateX = -showingSlideshowIndex*parseFloat(showingSlideshow.css("width"));
-  console.log(translateX);
+  if(direction=='left'){// && remainder==0){//+500px to transform left
+    if(showingMediaIndex==0){ //To the left of the slideshow
 
-  var siblingsMedia = showingSlideshow.find(".media");
+      if(showingContainerIndex==0){
+        showingContainerIndex=numberOfMediaContainer-1;// to the full begining of the slideshow
+        showingMediaIndex=0;
+      }
+      else{
+        showingContainerIndex--;
+        showingMediaIndex=-1;
+      }
+    }
+    else{
+      showingMediaIndex--;
+    }
+  }
+
+  showingContainer = mediasContainer.children().eq(showingContainerIndex);
+  //In the case left to several media -> last media to be shown
+  if(showingMediaIndex==-1){
+    numberMediaInContainer = showingContainer.find(".media").length;
+    //console.log(numberMediaInContainer);
+    showingMediaIndex=numberMediaInContainer-1;
+  }
+  //console.log(showingContainerIndex);
+  //console.log(showingMediaIndex);
+  //html objects to be shown
+
+  showingMedia = showingContainer.find(".media").eq(showingMediaIndex);
+  //Caluclating the new translation
+  translateX = -showingContainerIndex*parseFloat(showingContainer.css("width"));
+
+  var siblingsMedia = showingContainer.find(".media");
 
   for(var iter=0;iter<showingMediaIndex;iter++){
-    console.log(siblingsMedia.eq(iter).css("width"));
+    console.log(parseFloat(siblingsMedia.eq(iter).css("width")));
     translateX-=parseFloat(siblingsMedia.eq(iter).css("width"));
   }
 
 
   matrixString[matrixString.length-2] = ' '+translateX;
-  console.log(matrixString);
   mediasContainer.css("transform",matrixString.join(','));
-  mediaContainer.data("showingMediaIndexInSLideshow", showingMediaIndex);
-  mediaContainer.data("showingSlideshowIndex", showingSlideshow);
+  mediasContainer.data("showingMediaIndexInSLideshow", showingMediaIndex);
+  mediasContainer.data("showingContainerIndex", showingContainerIndex);
 
 };
 
@@ -98,12 +109,13 @@ function intializeTimers(){
 function fakeClick(element){
   element.click();
 }
-intializeTimers();
-
-initCarousels();
-$('video').click(function(){
-  this.paused ? this.play() : this.pause();
+$(function() {
+  //intializeTimers();
+  initCarousels();
 });
+  $('video').click(function(){
+    this.paused ? this.play() : this.pause();
+  });
 
 function resizeCaroussel(caroussel,width,height){
   //We assume the caroussel structure as in carousel.css
