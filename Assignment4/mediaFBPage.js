d41d09cb1ba51ab1b6c67bff76cc7bdfa125b5e5
Gautier Dervaux <gautier_dervaux@hotmail.com>
2018-01-24 13:06:25 +0100

    Comments OK except username No memory

--- Assignment4/mediaFBPage.js
+++ Assignment4/mediaFBPage.js
@@ -12,3 +12,31 @@ $(function() {
   });
 
 });
+
+$('#inputCommentButton').on("click",addComment);
+
+function addComment(){
+  var commentText = $(this).siblings('.textPost').val()
+  var commentsContainer = $(this).parent().parent().parent();
+  var firstComment = commentsContainer.children(".comment").first()
+  var newComment = firstComment.clone();
+  newComment.find(".commentText").html(commentText);
+  var commentsContainer = $(this).parent().parent().parent();
+  console.log(commentsContainer.attr("class"));
+  commentsContainer.css("height","1px");
+  newComment.insertBefore(firstComment);
+  commentsContainer.css("height","auto");
+  if(localStorage.profilePicSrc){
+    newComment.find(".avatarComment").attr("src",localStorage.profilePicSrc)
+  }
+
+}
+
+$(window).on("load",intializeAvatar)
+
+function intializeAvatar(){
+  console.log(localStorage.profilePicSrc);
+  if(localStorage.profilePicSrc){
+    $(".avatar").attr("src",localStorage.profilePicSrc)
+  }
+}
