f473e5ffee6c1ff57d12fe507acff8ca2bacc57b
Nathan Biette <nathan1.biette@free.fr>
2018-01-24 23:26:26 +0100

    Fixed the ckeckboxes

--- Assignment4/profile.js
+++ Assignment4/profile.js
@@ -5,12 +5,18 @@ var checkboxOfficial = document.querySelector("input[id=seeOfficial]");
 checkboxOfficial.addEventListener( 'change', function() {
     if(this.checked) {
         // Checkbox is checked..
-        document.getElementsByClassName("official firstOnglet")[0].style.display="inline";
+         var officials = document.querySelectorAll('.official');
+        Array.prototype.forEach.call(officials, function(elements, index) {
+          elements.style.display="inline";
+        });
         localStorage.officialChecked=1;
     } else {
 
         // Checkbox is not checked..
-        document.getElementsByClassName("official firstOnglet")[0].style.display="none";
+        var officials = document.querySelectorAll('.official');
+        Array.prototype.forEach.call(officials, function(elements, index) {
+          elements.style.display="none";
+        });
         localStorage.officialChecked=0;
     }
 });
@@ -20,16 +26,26 @@ var checkboxFan = document.getElementById("seeFan");
 checkboxFan.addEventListener( 'change', function() {
     if(this.checked) {
         // Checkbox is checked..
-        document.getElementsByClassName("fanArt firstOnglet")[0].style.display="inline";
+        var fanArts = document.querySelectorAll('.fanArt');
+        Array.prototype.forEach.call(fanArts, function(elements, index) {
+          elements.style.display="inline";
+        });
         localStorage.fanChecked=1;
-        alert(localStorage.fanChecked);
-    } else {
+      } else {
 
         // Checkbox is not checked..
-        document.getElementsByClassName("fanArt firstOnglet")[0].style.display="none";
+        var fanArts = document.querySelectorAll('.fanArt');
+        Array.prototype.forEach.call(fanArts, function(elements, index) {
+          elements.style.display="none";
+        });
         localStorage.fanChecked=0;
     }
 });
+
+$( "li" ).each(function( index ) {
+  console.log( index + ": " + $( this ).text() );
+});
+
 
 /*var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
 FileSaver.saveAs(blob, "hello world.txt");*/
